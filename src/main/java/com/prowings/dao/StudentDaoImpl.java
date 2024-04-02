f94469fe1ca516251e3f64f87644213a89492b23
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-04-02 12:11:42 +0530

    added getStudentList endpoint

--- src/main/java/com/prowings/dao/StudentDaoImpl.java
+++ src/main/java/com/prowings/dao/StudentDaoImpl.java
@@ -1,7 +1,9 @@
 package com.prowings.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -35,8 +37,24 @@ public class StudentDaoImpl implements StudentDao{
 	@Override
 	public List<Student> getStudentsList() {
 		System.out.println("inside getStudentList() of StudentDaoImpl");
-		return null;
-	}
+		
+		Session session = null;
+		Transaction txn = null;
+		List<Student> res = new ArrayList();
+		try {
+			session = sessionFactory.openSession();
+			txn = session.beginTransaction();
+			String hql = "FROM Student";
+			Query query = session.createQuery(hql);	
+			res = query.getResultList();
+			txn.commit();
+		}catch (Exception e) {
+			System.out.println("Error while fetching the student list!!");
+			e.printStackTrace();
+			if(txn != null)
+				txn.rollback();
+		}
 
-	
+		return res;
+	}	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,16 +3,29 @@
 
 <html>
 <head>
+
 <title>Add Student</title>
 </head>
 <body>
+	<div>
 
-	<h1>Add Student</h1>
-	<form  action="<c:url value='/students/save' />" method="post">
-	<label for="name">Name:</label> <input type="text" id="name"
-		   name="name"><br> <label for="address">Address:</label> <input
-		   type="text" id="address" name="address"><br> <input
-		   type="submit" value="Save">
-	</form>
+		<h1>All Student</h1>
+		<form action="<c:url value='/students/save' />" method="post">
+			<label for="name">Name:</label> <input type="text" id="name"
+				name="name"><br> <label for="address">Address:</label>
+			<input type="text" id="address" name="address"><br> <input
+				type="submit" value="Save">
+		</form>
+	</div>
+
+	<div>
+		<h1>Get All Students</h1>
+		<form action="<c:url value='/students/list' />" method="get">
+			<input type="submit" value="GetList">
+		</form>
+	</div>
+	
+	
+	
 </body>
 </html>
